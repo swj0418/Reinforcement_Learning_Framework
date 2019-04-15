34977536539e8b63621e89307958322420a25c28
Joel Michelson <joelmichelson@users.noreply.github.com>
2019-04-15 09:36:00 -0500

    add working antworld test
    no learning working yet

---
+++ tests/test_hysteretic_q_antworld.py
@@ -0,0 +1,38 @@
+'''
+testing hysteretic_q learning on the climbing game.
+'''
+
+from matplotlib import pyplot as plt
+
+from environments.env_antworld import Antworld
+
+from learning_algorithms.hysteretic_q import HystereticAgent
+
+
+
+if __name__ == '__main__':
+    env = Antworld()
+    epochs = 1
+    simulations = 1
+    epoch_length = 10
+    num_agents = 2
+
+    for simulation in range(simulations):
+
+        env = Antworld(agents_n=num_agents)
+        agents = [HystereticAgent(env,this_id) for this_id in range(num_agents)]
+
+        # get initial observations/rewards
+        observations, rewards, _, _ = env.step([],[x for x in range(num_agents)])
+        print(observations)
+
+        for epoch in range(epochs):
+            #reset world
+
+            for time in range(epoch_length):
+                env.display()
+                actions = []
+                for agent_num in range(num_agents):
+                    actions += [agents[agent_num].get_action_from_observation(observations[agent_num]),]
+
+                observations, rewards, _, _, = env.step(actions,[x for x in range(num_agents)])
